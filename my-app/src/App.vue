1c2c8290e51bea09ce42769c371a914e72005c40
gurielmp <gurielamp@gmail.com>
2019-04-03 18:10:58 +0700

    adding add button

--- my-app/src/App.vue
+++ my-app/src/App.vue
@@ -1,6 +1,7 @@
 <template>
  <div id="app">
   <Header />
+  <AddTodo v-on:add-todo="addTodo" />
   <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo"/>
  </div>
 </template>
@@ -8,12 +9,14 @@
 <script>
  import Header from './components/layout/Header';
  import Todos from './components/Todos';
+ import AddTodo from './components/AddTodo';
 
  export default {
   name: 'app',
   components: {
    Header,
-   Todos
+   Todos,
+   AddTodo
   },
   data () {
    return {
@@ -54,17 +57,36 @@
   methods: {
    deleteTodo(id) {
     this.todos = this.todos.filter(todo => todo.id !== id);
+   },
+   addTodo(newTodo) {
+    this.todos = [...this.todos, newTodo];
    }
   }
  }
 </script>
 
 <style>
- #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-  margin-top: 60px;
- }
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  body {
+    font-family: Arial, Helvetica, sans-serif;
+    line-height: 1.4;
+  }
+
+  .btn {
+    display: inline-block;
+    border: none;
+    background: #555;
+    color: #fff;
+    padding: 7px 20px;
+    cursor: pointer;
+  }
+
+  .btn:hover {
+    background: #666;
+  }
 </style>
